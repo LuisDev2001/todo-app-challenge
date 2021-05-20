55a0934e65aaa2c4d956404e948a0f59319c5074
LuiDev2001 <luisqrja@gmail.com>
2021-05-19 20:03:32 -0500

    Add express and script for express

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const serveStatic = require("serve-static");
+const path = require("path");
+const app = express();
+
+app.use("/", serveStatic(path.join(__dirname, "/dist")));
+const port = process.env.PORT || 8080;
+
+app.listen(port);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("/dist"));
+}
+
+app.get("*", (request, response) => {
+  response.sendFile(path.join(__dirname, "/dist", "index.html"));
+});
