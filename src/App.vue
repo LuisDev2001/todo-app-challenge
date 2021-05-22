813b045987498e062242a9efa9d561220d81b227
LuiDev2001 <luisqrja@gmail.com>
2021-05-21 21:18:09 -0500

    Create component form and styles

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
 <template>
   <h1 class="main-title">#todo</h1>
   <PxHeader />
+  <PxFormTask />
   <router-view />
 </template>
 
 <script>
 import PxHeader from "@/components/PxHeader";
+import PxFormTask from "@/components/PxFormTask";
+
 export default {
   components: {
     PxHeader,
+    PxFormTask,
   },
 };
 </script>
 
 <style lang="scss">
 @import "~@/assets/sass/_base.scss";
+#app {
+  width: 100%;
+  max-width: 610px;
+  margin: 0 auto;
+  padding: 0 15px;
+}
 </style>
---
+++ src/components/PxFormTask.vue
@@ -0,0 +1,20 @@
+<template>
+  <form action="" class="form">
+    <div class="form-group input">
+      <input type="text" placeholder="Add details" />
+    </div>
+    <div class="form-group">
+      <button type="submit">Add</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "PxFormTask",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "~@/assets/sass/components/_PxFormTask.scss";
+</style>
