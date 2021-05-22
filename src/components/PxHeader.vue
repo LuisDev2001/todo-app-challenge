3ab47546d748e17354a893a55e110863e7aa4e67
LuiDev2001 <luisqrja@gmail.com>
2021-05-21 21:01:47 -0500

    Implement component Header and styles

--- src/components/PxHeader.vue
+++ src/components/PxHeader.vue
@@ -1,15 +1,49 @@
 <template>
   <header class="header">
-    <router-link to="/">All</router-link>
-    <router-link to="/active-task">active task</router-link>
-    <router-link to="/completed-task">completed task</router-link>
+    <nav class="menu">
+      <ul class="menu__list">
+        <li class="menu__item" v-for="(link, index) in links" :key="index">
+          <router-link
+            :to="link.to"
+            class="menu__link"
+            v-text="link.linkText"
+          ></router-link>
+        </li>
+      </ul>
+    </nav>
   </header>
 </template>
 
 <script>
+import { reactive, toRefs } from "vue";
 export default {
   name: "PxHeader",
+  setup() {
+    //Array of objects for header link
+    const linksMenuState = reactive({
+      links: [
+        {
+          to: "/",
+          linkText: "All",
+        },
+        {
+          to: "/active-task",
+          linkText: "Active",
+        },
+        {
+          to: "/completed-task",
+          linkText: "Completed",
+        },
+      ],
+    });
+
+    return {
+      ...toRefs(linksMenuState),
+    };
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import "~@/assets/sass/components/_PxHeader.scss";
+</style>
