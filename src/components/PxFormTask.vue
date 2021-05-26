b117b1656a99e7bd05f7d32fa132b9a9ec13be50
LuiDev2001 <luisqrja@gmail.com>
2021-05-25 23:38:49 -0500

    Add method for post data of form

--- src/components/PxFormTask.vue
+++ src/components/PxFormTask.vue
@@ -1,7 +1,12 @@
 <template>
-  <form action="" class="form">
+  <form action="" class="form" @submit.prevent="submitTask">
     <div class="form-group input">
-      <input type="text" placeholder="Add details" />
+      <input
+        type="text"
+        autocomplete="off"
+        placeholder="Add details"
+        v-model="task"
+      />
     </div>
     <div class="form-group">
       <button type="submit">Add</button>
@@ -10,8 +15,39 @@
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
   name: "PxFormTask",
+  setup() {
+    let task = ref("");
+
+    const submitTask = async () => {
+      try {
+        const API = "https://api-fake-todo-app-2021.herokuapp.com/task";
+        const config = {
+          method: "POST",
+          body: JSON.stringify({
+            task: task.value,
+            status: false,
+          }),
+          headers: {
+            "Content-type": "application/json; charset=UTF-8",
+          },
+        };
+        const response = await fetch(API, config);
+        const data = await response.json();
+        task.value = "";
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    return {
+      submitTask,
+      task,
+    };
+  },
 };
 </script>
 
