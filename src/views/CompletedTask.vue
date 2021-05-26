0d688501ccaa2bca5368ac65490ce9cde44b2850
LuiDev2001 <luisqrja@gmail.com>
2021-05-25 22:10:30 -0500

    Fetch data to completed task of view Completed.vue

--- src/views/CompletedTask.vue
+++ src/views/CompletedTask.vue
@@ -1,17 +1,46 @@
 <template>
-  <section>
-    <h1>Completed task</h1>
-    <!-- <PxContainerResult /> -->
+  <section class="completed">
+    <PxContainerResult :itemsTodo="dataCompletedTask" />
   </section>
 </template>
 
 <script>
+import { onMounted, reactive, toRefs } from "vue";
+
 import PxContainerResult from "@/components/PxContainerResult";
 
 export default {
   name: "CompletedTask",
   components: {
-    // PxContainerResult,
+    PxContainerResult,
+  },
+  setup() {
+    const dataTodoListCompletedState = reactive({
+      dataCompletedTask: [],
+    });
+
+    const getAllCompletedTask = async () => {
+      try {
+        const API = "https://api-fake-todo-app-2021.herokuapp.com/task";
+        const getData = await fetch(API);
+        const response = await getData.json();
+        for (const task of response) {
+          if (task.status) {
+            dataTodoListCompletedState.dataCompletedTask.push(task);
+          }
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    onMounted(async () => {
+      await getAllCompletedTask();
+    });
+
+    return {
+      ...toRefs(dataTodoListCompletedState),
+    };
   },
 };
 </script>
